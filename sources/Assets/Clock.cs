b953c917037433da78f6dcc3c570850fbaba9e9c
SaguchiWataru <65752137+SaguchiWataru@users.noreply.github.com>
2021-04-16 20:28:49 +0900

    Create Clock.cs

---
+++ sources/Assets/Clock.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Clock : MonoBehaviour {
+
+	private Text ClockText;
+
+	// Use this for initialization
+	void Start () {
+		ClockText = GetComponentInChildren<Text>();
+
+	}
+
+	// Update is called once per frame
+	void Update () {
+		ClockText.text = DateTime.Now.ToString("HH:mm:ss");
+
+	}
+}
